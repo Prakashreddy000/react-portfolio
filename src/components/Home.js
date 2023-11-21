e7ef437a4773a5435c9d3bd1a6e334c1dd4a002e
Prakashreddy000 <prakashreddymunigala22@gmail.com>
2023-11-21 21:39:14 +0530

    portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { useTypewriter } from 'react-simple-typewriter';
 import imagee from '../assests/mobile img.jpeg';
 import {  BiSolidRightArrowAlt } from "react-icons/bi";
 import { Link } from 'react-scroll';
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ['Frontend Developer .', 'React Developer .'],
+    loop:{},
+    typeSpeed: 100,
+    deleteSpeed: 40
+  });
+
   return (
    
       <div 
@@ -12,8 +19,8 @@ const Home = () => {
       className='h-screen  w-full bg-gradient-to-b from-black to-slate-500'>
         <div className='max-w-screen-lg  mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col  justify-center h-full'>
-          <h2 className='text-5xl sm:text-xl font-bold text-white' >I'm a Frontend Developer</h2>
-          <p className='py-3 font-semibold max-w-md'>I have built this portfolio using React.js Lehjsfgsihgfisuargiuelhg4 o4rfgiuegfiu4egafiuiueaf 9a uyfecuygbv</p>
+          <h2 className='text-5xl sm:text-xl font-bold text-white' >I'm a <span className='text-green-400'>{text}</span></h2>
+          <p className='py-3 font-semibold max-w-md'>I have built this portfolio using <span className='text-white'>React js and Tailwind css</span></p>
           <div>
             <Link to='Experience' smooth duration={500} className='group text-black w-fit px-2 py-1 m-2 bg-gradient-to-r from-cyan-300 to-blue-200 flex rounded-lg'>
               Experience
