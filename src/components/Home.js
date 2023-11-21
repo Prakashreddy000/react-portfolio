cf212c9d661165943172426141368f0a000a4f29
Prakashreddy000 <prakashreddymunigala22@gmail.com>
2023-11-21 21:20:51 +0530

    portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import image from '../assests/image.png';
+
+import imagee from '../assests/mobile img.jpeg';
 import {  BiSolidRightArrowAlt } from "react-icons/bi";
 import { Link } from 'react-scroll';
 
@@ -8,10 +9,10 @@ const Home = () => {
    
       <div 
       name="Home"
-      className='h-screen w-full bg-gradient-to-b from-black to-slate-500'>
+      className='h-screen  w-full bg-gradient-to-b from-black to-slate-500'>
         <div className='max-w-screen-lg  mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col  justify-center h-full'>
-          <h2 className='text-3xl sm:text-xl font-bold text-white' >I'm a Frontend Developer</h2>
+          <h2 className='text-5xl sm:text-xl font-bold text-white' >I'm a Frontend Developer</h2>
           <p className='py-3 font-semibold max-w-md'>I have built this portfolio using React.js Lehjsfgsihgfisuargiuelhg4 o4rfgiuegfiu4egafiuiueaf 9a uyfecuygbv</p>
           <div>
             <Link to='Experience' smooth duration={500} className='group text-black w-fit px-2 py-1 m-2 bg-gradient-to-r from-cyan-300 to-blue-200 flex rounded-lg'>
@@ -23,8 +24,8 @@ const Home = () => {
         </div>
         <div>
             <img alt='my pic' 
-            className='w-2/3 mx-auto md:w-full'
-            src={image}/>
+            className='w-2/4 pb-3 mx-auto md:w-2/4 rounded-2xl'
+            src={imagee}/>
           </div>
       </div>
     </div>
